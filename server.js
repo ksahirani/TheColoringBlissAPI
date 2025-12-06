ba73a12b936426f1eb92b5d92def6d9981342e04
ksahirani <kenonsahirani07@gmail.com>
2025-12-07 01:53:17 +0800

    There's changes in the server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://the-coloring-bliss-front-end.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
